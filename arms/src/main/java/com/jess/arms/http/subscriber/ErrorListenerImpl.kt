bbf26377009e22bb145c44adfad5a8299d785491
guanzhirui <guanzhirui@gmail.com>
2021-12-30 11:24:25 +0800

    release : 1.1.18

--- arms/src/main/java/com/jess/arms/http/subscriber/ErrorListenerImpl.kt
+++ arms/src/main/java/com/jess/arms/http/subscriber/ErrorListenerImpl.kt
@@ -1,6 +1,7 @@
 package com.jess.arms.http.subscriber
 
 import android.content.Context
+import com.google.gson.JsonIOException
 import com.google.gson.JsonParseException
 import com.jess.arms.http.convert.HandlerErrorGsonResponseBodyConverter
 import com.jess.arms.integration.AppManager
@@ -24,68 +25,45 @@ class ErrorListenerImpl : ResponseErrorListener{
     override fun handleResponseError(context: Context?, e: Throwable?) {
         Timber.tag("Catch-Error").w(e)
 
-        if (e is NetErrorException){
-
+        var message = "未知错误"
+        if (e is UnknownHostException){
+            message = "无连接异常"
+        } else if (e is SocketTimeoutException){
+            message = "请求网络超时"
+        } else if (e is HttpException){
+            message = convertStatusCode(e)
+        } else if (e is JsonParseException
+            || e is android.net.ParseException
+            || e is JSONException
+            || e is JsonIOException
+            || e is ParseException) {
+            message = "数据解析错误"
+        } else if (e is ConnectException){
+            message = "无法连接到服务器"
         }
 
+        AppManager.getAppManager().showSnackbar(message, true)
+    }
 
-
-
-        var error: NetErrorException? = null
-        // 对不是自定义抛出的错误进行解析
-        if (e !is NetErrorException) {
-            if (e is UnknownHostException) {
-                error = NetErrorException(e, NetErrorException.NO_CONNECT_ERROR)
-            } else if (e is JSONException || e is JsonParseException || e is ParseException) {
-                error = NetErrorException(e, NetErrorException.PARSE_ERROR)
-            } else if (e is SocketTimeoutException) {
-                error = NetErrorException(e, NetErrorException.SOCKET_TIMEOUT_ERROR)
-            } else if (e is ConnectException) {
-                error = NetErrorException(e, NetErrorException.CONNECT_EXCEPTION_ERROR)
-            } else if (e is HttpException) {
-                // 输出服务器返回的错误信息
-                if (e.response() != null && e.response()!!.errorBody() != null) {
-                    e.response()?.errorBody()?.let {
-                        try {
-                            val json = JSONObject(it.string())
-                            val errorMessage =
-                                json.optString(HandlerErrorGsonResponseBodyConverter.MESSAGE)
-                            error = if (!errorMessage.isNullOrEmpty()) {
-                                NetErrorException(
-                                    errorMessage,
-                                    NetErrorException.HTTP_EXCEPTION
-                                )
-                            } else {
-                                NetErrorException(
-                                    e,
-                                    NetErrorException.HTTP_EXCEPTION
-                                )
-                            }
-                        } catch (e: Exception) {
-                            e.printStackTrace()
-                            error = NetErrorException(
-                                e,
-                                NetErrorException.HTTP_EXCEPTION
-                            )
-                        }
-                    }
-                } else {
-                    error = NetErrorException(e, NetErrorException.HTTP_EXCEPTION)
-                }
-            } else {
-                error = NetErrorException(e, NetErrorException.OTHER)
+    private fun convertStatusCode(httpException: HttpException): String {
+        val msg: String = when {
+            httpException.code() == 500 -> {
+                "服务器发生错误"
             }
-        } else {
-            error = if (e is HttpException) {
-                NetErrorException(e, e.code())
-            } else {
-                NetErrorException(e.message, e.mErrorType)
+            httpException.code() == 404 -> {
+                "请求地址不存在"
+            }
+            httpException.code() == 403 -> {
+                "请求被服务器拒绝"
+            }
+            httpException.code() == 307 -> {
+                "请求被重定向到其他页面"
+            }
+            else -> {
+                httpException.message()
             }
         }
-
-        error?.let {
-            AppManager.getAppManager().showSnackbar(error?.message, false)
-        }
+        return msg
     }
 
 }
