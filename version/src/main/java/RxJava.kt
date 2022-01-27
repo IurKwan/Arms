b403d3f0582589d89409ddd78b633c7b2aacd7a8
guanzhirui <guanzhirui@gmail.com>
2022-01-27 13:35:30 +0800

    feat: 取消rxpermission依赖使用

--- version/src/main/java/RxJava.kt
+++ version/src/main/java/RxJava.kt
@@ -7,7 +7,6 @@ object RxJava {
     private const val version = "3.0.13"
     const val rxjava3 = "io.reactivex.rxjava3:rxjava:$version"
     const val rxAndroid3 = "io.reactivex.rxjava3:rxandroid:3.0.0"
-    const val rxPermissions = "com.github.tbruyelle:rxpermissions:0.12"
     const val rxerrorhandler2 = "me.jessyan:rxerrorhandler:2.1.1"
 
 
