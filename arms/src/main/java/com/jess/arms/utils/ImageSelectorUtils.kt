ae6720c45fc23981557b98c6f7c8dcea9fb364a8
guanzhirui <guanzhirui@gmail.com>
2022-04-12 17:52:40 +0800

    feat: 优化选择图库逻辑

--- arms/src/main/java/com/jess/arms/utils/ImageSelectorUtils.kt
+++ arms/src/main/java/com/jess/arms/utils/ImageSelectorUtils.kt
@@ -2,33 +2,24 @@ package com.jess.arms.utils
 
 import android.app.Activity
 import android.content.Context
-import android.net.Uri
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.jess.arms.R
 import com.luck.picture.lib.animators.AnimationType
+import com.luck.picture.lib.basic.PictureSelectionModel
 import com.luck.picture.lib.basic.PictureSelector
 import com.luck.picture.lib.config.PictureMimeType
 import com.luck.picture.lib.config.PictureSelectionConfig.selectorStyle
-import com.luck.picture.lib.config.SelectMimeType
-import com.luck.picture.lib.config.SelectModeConfig
-import com.luck.picture.lib.engine.CompressFileEngine
-import com.luck.picture.lib.engine.CropFileEngine
 import com.luck.picture.lib.engine.ImageEngine
 import com.luck.picture.lib.engine.UriToFileTransformEngine
 import com.luck.picture.lib.entity.LocalMedia
 import com.luck.picture.lib.interfaces.OnKeyValueResultCallbackListener
-import com.luck.picture.lib.interfaces.OnResultCallbackListener
 import com.luck.picture.lib.style.PictureSelectorStyle
 import com.luck.picture.lib.style.SelectMainStyle
 import com.luck.picture.lib.style.TitleBarStyle
-import com.luck.picture.lib.utils.DateUtils
 import com.luck.picture.lib.utils.SandboxTransformUtils
 import com.luck.picture.lib.utils.StyleUtils
 import com.yalantis.ucrop.UCrop
-import top.zibin.luban.Luban
-import top.zibin.luban.OnNewCompressListener
-import java.io.File
 
 /**
  * 选择图片库
@@ -36,58 +27,20 @@ import java.io.File
  * @author IurKwan
  * @date 1/19/21
  */
-class ImageSelectorUtils private constructor() {
-
-    companion object {
-        val instance = ImageSelectorUtils.holder
-    }
-
-    private object ImageSelectorUtils {
-        val holder = ImageSelectorUtils()
-    }
+object ImageSelectorUtils {
 
     private var mEngine: ImageEngine? = null
-    private var mCutEngine: CropFileEngine? = null
-
     private var mTheme: PictureSelectorStyle = PictureSelectorStyle()
 
-    fun init(theme: PictureSelectorStyle, engine: ImageEngine, cutEngine: CropFileEngine) {
+    fun init(theme: PictureSelectorStyle, engine: ImageEngine) {
         mTheme = theme
         mEngine = engine
-        mCutEngine = cutEngine
     }
 
-    fun getEngine(): ImageEngine {
-        return mEngine!!
-    }
-
-    fun getTheme(): PictureSelectorStyle {
-        return mTheme
-    }
-
-    fun takeOnePhoto(
-        fragment: Fragment,
-        isCut: Boolean,
-        listener: OnResultCallbackListener<LocalMedia>
-    ) {
-        checkTheme()
-
-        PictureSelector.create(fragment)
-            .openGallery(SelectMimeType.ofImage())
-            .setSelectionMode(SelectModeConfig.SINGLE)
-            .setSelectedData(null)
-            .setMaxSelectNum(9)
-            .setMinSelectNum(1)
-            .setMaxVideoSelectNum(9)
-            .setMinVideoSelectNum(1)
-            .setRecordVideoMaxSecond(15)
-            .setRecordVideoMinSecond(3)
-
-            .setImageEngine(mEngine)
+    fun defaultModel(model: PictureSelectionModel): PictureSelectionModel {
+        return model.setImageEngine(mEngine)
             .setSelectorUIStyle(mTheme)
-            .setImageSpanCount(3)
             .setCompressEngine(ImageFileCompressEngine())
-            .setCropEngine(mCutEngine)
             .setSandboxFileEngine(SandboxFileEngine())
             .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)
             .isDisplayCamera(true)
@@ -110,215 +63,6 @@ class ImageSelectorUtils private constructor() {
             .isFastSlidingSelect(true)
             .isDirectReturnSingle(true)
             .setSkipCropMimeType(PictureMimeType.ofGIF(), PictureMimeType.ofWEBP())
-            .forResult(listener)
-    }
-
-    fun starCamera(fragment: Fragment, listener: OnResultCallbackListener<LocalMedia>) {
-        checkTheme()
-
-        PictureSelector.create(fragment)
-            .openCamera(SelectMimeType.ofImage())
-            .setCropEngine(mCutEngine)
-            .setCompressEngine(ImageFileCompressEngine())
-            .forResult(listener)
-    }
-
-    fun startPictureSelected(
-        fragment: Fragment,
-        mLocalMedia: List<LocalMedia>,
-        max: Int,
-        isPhoto: Boolean,
-        listener: OnResultCallbackListener<LocalMedia>
-    ) {
-        checkTheme()
-
-        PictureSelector.create(fragment)
-            .openGallery(if (isPhoto) SelectMimeType.ofImage() else SelectMimeType.ofVideo())
-            .setSelectionMode(SelectModeConfig.SINGLE)
-            .setSelectedData(mLocalMedia)
-            .setMaxSelectNum(if (max == 0) 1 else max)
-            .setMinSelectNum(1)
-            .setMaxVideoSelectNum(999)
-            .setMinVideoSelectNum(1)
-            .setRecordVideoMaxSecond(15)
-            .setRecordVideoMinSecond(1)
-
-            .setImageEngine(mEngine)
-            .setSelectorUIStyle(mTheme)
-            .setImageSpanCount(3)
-            .setCompressEngine(ImageFileCompressEngine())
-            .setCropEngine(mCutEngine)
-            .setSandboxFileEngine(SandboxFileEngine())
-            .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)
-            .isDisplayCamera(true)
-            .isPageStrategy(true)
-            .isGif(true)
-            .isOpenClickSound(false)
-            .isPreviewAudio(false)
-            .isPreviewImage(true)
-            .isPreviewVideo(true)
-            .isPreviewFullScreenMode(true)
-            .isEmptyResultReturn(true)
-            .isWithSelectVideoImage(false)
-            .isSelectZoomAnim(true)
-            .isCameraAroundState(true)
-            .isCameraRotateImage(true)
-            .isWebp(true)
-            .isBmp(true)
-            .isMaxSelectEnabledMask(true)
-            .isAutomaticTitleRecyclerTop(true)
-            .isFastSlidingSelect(true)
-            .isDirectReturnSingle(true)
-            .setSkipCropMimeType(PictureMimeType.ofGIF(), PictureMimeType.ofWEBP())
-            .forResult(listener)
-    }
-
-    fun startPictureSelected(
-        fragment: Fragment,
-        mLocalMedia: List<LocalMedia>,
-        limitSize: Boolean,
-        isPhoto: Boolean,
-        listener: OnResultCallbackListener<LocalMedia>
-    ) {
-        checkTheme()
-
-        PictureSelector.create(fragment)
-            .openGallery(if (isPhoto) SelectMimeType.ofImage() else SelectMimeType.ofVideo())
-            .setSelectionMode(SelectModeConfig.SINGLE)
-            .setSelectedData(mLocalMedia)
-            .setMaxSelectNum(if (limitSize) 9 else 999)
-            .setMinSelectNum(1)
-            .setMaxVideoSelectNum(999)
-            .setMinVideoSelectNum(1)
-            .setRecordVideoMaxSecond(15)
-            .setRecordVideoMinSecond(1)
-
-            .setImageEngine(mEngine)
-            .setSelectorUIStyle(mTheme)
-            .setImageSpanCount(3)
-            .setCompressEngine(ImageFileCompressEngine())
-            .setCropEngine(mCutEngine)
-            .setSandboxFileEngine(SandboxFileEngine())
-            .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)
-            .isDisplayCamera(true)
-            .isPageStrategy(true)
-            .isGif(true)
-            .isOpenClickSound(false)
-            .isPreviewAudio(false)
-            .isPreviewImage(true)
-            .isPreviewVideo(true)
-            .isPreviewFullScreenMode(true)
-            .isEmptyResultReturn(true)
-            .isWithSelectVideoImage(false)
-            .isSelectZoomAnim(true)
-            .isCameraAroundState(true)
-            .isCameraRotateImage(true)
-            .isWebp(true)
-            .isBmp(true)
-            .isMaxSelectEnabledMask(true)
-            .isAutomaticTitleRecyclerTop(true)
-            .isFastSlidingSelect(true)
-            .isDirectReturnSingle(true)
-            .setSkipCropMimeType(PictureMimeType.ofGIF(), PictureMimeType.ofWEBP())
-            .forResult(listener)
-    }
-
-    fun startPictureSelected(
-        activity: Activity,
-        mLocalMedia: List<LocalMedia>,
-        limitSize: Boolean,
-        isPhoto: Boolean,
-        listener: OnResultCallbackListener<LocalMedia>
-    ) {
-        checkTheme()
-
-        PictureSelector.create(activity)
-            .openGallery(if (isPhoto) SelectMimeType.ofImage() else SelectMimeType.ofVideo())
-            .setSelectionMode(SelectModeConfig.SINGLE)
-            .setSelectedData(mLocalMedia)
-            .setMaxSelectNum(if (limitSize) 9 else 999)
-            .setMinSelectNum(1)
-            .setMaxVideoSelectNum(999)
-            .setMinVideoSelectNum(1)
-            .setRecordVideoMaxSecond(15)
-            .setRecordVideoMinSecond(1)
-
-            .setImageEngine(mEngine)
-            .setSelectorUIStyle(mTheme)
-            .setImageSpanCount(3)
-            .setCompressEngine(ImageFileCompressEngine())
-            .setCropEngine(mCutEngine)
-            .setSandboxFileEngine(SandboxFileEngine())
-            .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)
-            .isDisplayCamera(true)
-            .isPageStrategy(true)
-            .isGif(true)
-            .isOpenClickSound(false)
-            .isPreviewAudio(false)
-            .isPreviewImage(true)
-            .isPreviewVideo(true)
-            .isPreviewFullScreenMode(true)
-            .isEmptyResultReturn(true)
-            .isWithSelectVideoImage(false)
-            .isSelectZoomAnim(true)
-            .isCameraAroundState(true)
-            .isCameraRotateImage(true)
-            .isWebp(true)
-            .isBmp(true)
-            .isMaxSelectEnabledMask(true)
-            .isAutomaticTitleRecyclerTop(true)
-            .isFastSlidingSelect(true)
-            .isDirectReturnSingle(true)
-            .setSkipCropMimeType(PictureMimeType.ofGIF(), PictureMimeType.ofWEBP())
-            .forResult(listener)
-    }
-
-    fun startMediaSelected(
-        activity: Activity,
-        mLocalMedia: List<LocalMedia>,
-        listener: OnResultCallbackListener<LocalMedia>
-    ) {
-        checkTheme()
-
-        PictureSelector.create(activity)
-            .openGallery(SelectMimeType.ofAll())
-            .setSelectionMode(SelectModeConfig.SINGLE)
-            .setSelectedData(mLocalMedia)
-            .setMaxSelectNum(9)
-            .setMinSelectNum(1)
-            .setMaxVideoSelectNum(1)
-            .setMinVideoSelectNum(1)
-            .setRecordVideoMaxSecond(15)
-            .setRecordVideoMinSecond(1)
-
-            .setImageEngine(mEngine)
-            .setSelectorUIStyle(mTheme)
-            .setImageSpanCount(3)
-            .setCompressEngine(ImageFileCompressEngine())
-            .setCropEngine(mCutEngine)
-            .setSandboxFileEngine(SandboxFileEngine())
-            .setRecyclerAnimationMode(AnimationType.DEFAULT_ANIMATION)
-            .isDisplayCamera(true)
-            .isPageStrategy(true)
-            .isGif(true)
-            .isOpenClickSound(false)
-            .isPreviewAudio(false)
-            .isPreviewImage(true)
-            .isPreviewVideo(true)
-            .isPreviewFullScreenMode(true)
-            .isEmptyResultReturn(true)
-            .isWithSelectVideoImage(false)
-            .isSelectZoomAnim(true)
-            .isCameraAroundState(true)
-            .isCameraRotateImage(true)
-            .isWebp(true)
-            .isBmp(true)
-            .isMaxSelectEnabledMask(true)
-            .isAutomaticTitleRecyclerTop(true)
-            .isFastSlidingSelect(true)
-            .isDirectReturnSingle(true)
-            .setSkipCropMimeType(PictureMimeType.ofGIF(), PictureMimeType.ofWEBP())
-            .forResult(listener)
     }
 
     fun getFinalPath(media: LocalMedia): String {
@@ -332,8 +76,6 @@ class ImageSelectorUtils private constructor() {
     }
 
     fun openExternalPreview(fragment: Fragment, position: Int, media: ArrayList<LocalMedia>) {
-        checkTheme()
-
         PictureSelector.create(fragment)
             .openPreview()
             .setImageEngine(mEngine)
@@ -343,8 +85,6 @@ class ImageSelectorUtils private constructor() {
     }
 
     fun openExternalPreview(activity: Activity, position: Int, media: ArrayList<LocalMedia>) {
-        checkTheme()
-
         PictureSelector.create(activity)
             .openPreview()
             .setImageEngine(mEngine)
@@ -353,41 +93,7 @@ class ImageSelectorUtils private constructor() {
             .startActivityPreview(position, false, media)
     }
 
-    private fun checkTheme() {
-        if (mEngine == null) {
-            throw RuntimeException("请先初始化主题")
-        }
-    }
-
-    private class ImageFileCompressEngine : CompressFileEngine {
-        override fun onStartCompress(
-            context: Context,
-            source: ArrayList<Uri>,
-            call: OnKeyValueResultCallbackListener?
-        ) {
-            Luban.with(context)
-                .load(source)
-                .ignoreBy(100)
-                .setRenameListener { filePath ->
-                    val indexOf = filePath.lastIndexOf(".")
-                    val postfix = if (indexOf != -1) filePath.substring(indexOf) else ".jpg"
-                    DateUtils.getCreateFileName("CMP_") + postfix
-                }
-                .setCompressListener(object : OnNewCompressListener {
-                    override fun onStart() {}
-                    override fun onSuccess(source: String, compressFile: File) {
-                        call?.onCallback(source, compressFile.absolutePath)
-                    }
-
-                    override fun onError(source: String, e: Throwable) {
-                        call?.onCallback(source, null)
-                    }
-                })
-                .launch()
-        }
-    }
-
-    private fun buildOptions(context: Context): UCrop.Options {
+    fun buildOptions(context: Context): UCrop.Options {
         val options = UCrop.Options()
         options.setHideBottomControls(false)
         options.setFreeStyleCropEnabled(false)
@@ -441,7 +147,7 @@ class ImageSelectorUtils private constructor() {
         return options
     }
 
-    private class SandboxFileEngine : UriToFileTransformEngine {
+    class SandboxFileEngine : UriToFileTransformEngine {
         override fun onUriToFileAsyncTransform(
             context: Context,
             srcPath: String,
@@ -454,5 +160,4 @@ class ImageSelectorUtils private constructor() {
             )
         }
     }
-
 }
